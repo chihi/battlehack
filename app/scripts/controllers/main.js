cabcd3aedce8b1d73662d4a7f01b7382e1c0be57
Xier <xier.ongzx@gmail.com>
2015-03-29 01:06:58 +0800

    site update

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,26 +10,29 @@
 angular.module('battlehackApp')
   .controller('MainCtrl', function ($scope, appService) {
     
+  	$scope.merchantList = [];
 
-    $scope.merchantList = [
-		{
-			"id":1,
-			"name" : 'Swee Choon',
-			"description" : 'Cras justo odio, dapibus ac facilisis in. montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.',
-			"phone_number": '12313123',
-			"email" : 'sasdas',
-			"queing_no":'10'
+  	$scope.merchantList = appService.getMerchant();
 
-		},
-		{
-			"id":2,
-			"name" : 'KFC',
-			"description" : 'Fast food place',
-			"phone_number": '12313123',
-			"email" : 'sasdas',
-			"queing_no":'7'
-		}
+  //   $scope.merchantList = [
+		// {
+		// 	"id":1,
+		// 	"name" : 'Swee Choon',
+		// 	"description" : 'Cras justo odio, dapibus ac facilisis in. montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.',
+		// 	"phone_number": '12313123',
+		// 	"email" : 'sasdas',
+		// 	"queing_no":'10'
 
-    ];
+		// },
+		// {
+		// 	"id":2,
+		// 	"name" : 'KFC',
+		// 	"description" : 'Fast food place',
+		// 	"phone_number": '12313123',
+		// 	"email" : 'sasdas',
+		// 	"queing_no":'7'
+		// }
+
+  //   ];
 
   });
