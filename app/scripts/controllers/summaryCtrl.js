241bffbeb2e4ca92b6f319c7402875c1c84ace37
Xier <xier.ongzx@gmail.com>
2015-03-29 00:57:30 +0800

    update

---
+++ app/scripts/controllers/summaryCtrl.js
@@ -0,0 +1,18 @@
+
+	'use strict';
+
+/**
+ * @ngdoc function
+ * @name battlehackApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the battlehackApp
+ */
+angular.module('battlehackApp')
+  .controller('SummaryCtrl', function ($scope, appService) {
+    
+
+   
+   
+
+  });
